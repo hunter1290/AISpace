4c0b91c89d8f4fe4278182cef7255565980773fd
hunter1290 <tusharmishra020@gmail.com>
2024-03-06 19:49:18 +0530

    chatgpt initiate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch,Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
--- frontend/src/components/tryModel/ChatGPT.jsx
+++ frontend/src/components/tryModel/ChatGPT.jsx
@@ -1,23 +1,118 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
 import Navbar from '../Navbar'
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: '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',
+  dangerouslyAllowBrowser: true  // This is the default and can be omitted
+});
 
 const Main = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ background-color: #030212c0;
+ color:white;
+ `;
+ const Container = styled.div`
+ height: 100%;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ `;
+ const Info = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+   img{
+    height: 30vh;
+    width: 15vw;
+    border-radius: 50%;
+    margin-top: 3%;
+   }
+   p{
+    margin-top: 3%;
+    text-align: justify;
+    height: 30vh;
+    width: 70vw;
+   }
+ `;
+const Work = styled.div`
+ border: 2px solid;
 `;
-const Bottom = styled.div`
+const Chat = styled.div`
+  height: 80vh;
+  width: 60vw;
+  padding: 3%;
+  text-align: justify;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
 `;
-const Up = styled.div``;
-const Down = styled.div``;
+const Send = styled.div`
+display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2%;
+input{
+  color: white;
+  background-color: black;
+  width: 45vw;
+  height: 3vh;
+  padding: 3%;
+}
+`;
 
 function ChatGPT() {
+  
+  const [chatInput,setChatInput] = useState('');
+  const [chat,setChat] = useState(['Start your chat'])
+
+  const chatCompletion = async()=>{
+    const completion = await openai.chat.completions.create({
+			messages: [{ role: "system", content: chatInput}],
+			model: "gpt-3.5-turbo",
+		  });
+		  const newRes = completion.choices[0].message.content; 
+     var data =  chat;
+     data.push(newRes);
+     setChat(data);
+      console.log(chatInput);
+      console.log(newRes);
+  }
+
+  const handleChatInput = (event) => {
+    setChatInput(event.target.value)
+  };
 
   return (
     <Main>
-       <Navbar/>
-       Current functionality under development you can try this model on official website
-       <br />
-       <a href="https://chat.openai.com/" target="_blank">Click Here</a>
+       <Navbar/> 
+        changes are going on.....
+       <Container>
+         <Info>
+                <img src="https://cdn.sanity.io/images/2ej9qrfy/production/73d095f3497e7e34069ccc5b2e2f75b210578742-1024x486.jpg" alt="img" />
+                <p>Chatbots are one of the most exciting applications of LLMs, and ChatGPT is a great example of this. Powered by the GPT-4 language model, ChatGPT can engage in natural language conversations with users.</p>
+         </Info>
+         <Work>
+           <Chat>
+                {
+                  chat.map((x,key)=>(
+                    <>{x}<br/></>
+                    
+                  ))
+                }
+           </Chat>
+           <Send>
+             <input type="text" value={chatInput} placeholder='start your chat' onChange={handleChatInput}/>
+             <button onClick={chatCompletion}>send</button>
+           </Send>
+         </Work>
+       </Container>
     </Main>
   )
 }
